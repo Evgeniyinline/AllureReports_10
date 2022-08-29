782b11c076ca74cc8bac8620d36ea795d8b82f76
Evgeniy <longlimp@gmail.com>
2022-08-29 21:56:15 +0200

    added Aftereach and fixed gitignore

--- src/test/java/allure/tests/TestBase.java
+++ src/test/java/allure/tests/TestBase.java
@@ -1,6 +1,8 @@
 package allure.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -8,5 +10,10 @@ public class TestBase {
     static void configure() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1080x1080";
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        Selenide.closeWebDriver();
     }
 }
